8ca228faf6c0a24f56e3ea7df90614b1b4d08b8d
mauahuma <ma.ahumadac@duocuc.cl>
2022-09-30 22:46:47 -0300

    Commit Usuarios inicial

---
+++ rsxxi/src/components/Workers/Users/AddEditUserForm/AddEditUserForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "semantic-ui-react";
+import "./AddEditUserForm";
+export function AddEditUserForm() {
+  return (
+    <div>
+      <h2>AddEditUserForm</h2>
+    </div>
+  );
+}
--- rsxxi/src/routes/routes.workers.js
+++ rsxxi/src/routes/routes.workers.js
@@ -1,10 +1,22 @@
 import { WorkersLayout } from "../layouts";
-import { HomeAdmin } from "../Pages/Workers";
+import { HomeAdmin, UsersWorker, PedidosWorkers } from "../Pages/Workers";
 const routesWorkers = [
   {
     path: "/Workers",
     layout: WorkersLayout,
     component: HomeAdmin,
+    exact: true,
+  },
+  {
+    path: "/Workers/Usuarios",
+    layout: WorkersLayout,
+    component: UsersWorker,
+    exact: true,
+  },
+  {
+    path: "/Workers/Pedidos",
+    layout: WorkersLayout,
+    component: PedidosWorkers,
     exact: true,
   },
 ];
