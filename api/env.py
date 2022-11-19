5fe024451414aae8d8cbc29e26febaa751761d8b
mauahuma <ma.ahumadac@duocuc.cl>
2022-11-18 22:57:38 -0300

    gitignore

--- api/env.py
+++ api/env.py
@@ -4,17 +4,11 @@ from os import environ
 os.environ['DJANGO_SUPERUSER_EMAIL'] = 'admin@test.com'
 os.environ['DJANGO_SUPERUSER_USERNAME'] = 'admin'
 os.environ['DJANGO_SUPERUSER_PASSWORD'] = 'admin'
-os.environ['SECRET_KEY'] = d69ps55v0d3cn7x395y6dev-28cj*&@+okq5a%0@k0$chb&s1t
-os.environ['BD_NAME']= XE
-os.environ['BD_USER'] =system
+os.environ['SECRET_KEY'] = 'd69ps55v0d3cn7x395y6dev-28cj*&@+okq5a%0@k0$chb&s1t'
+os.environ['BD_NAME'] = 'XE'
+os.environ['BD_USER'] = 'system'
 
-os.environ['BD_PASSWORD']=202227821Yuli
+os.environ['BD_PASSWORD'] = 'xxxxxxxxx'
 
-os.environ['BD_HOST']=localhost
-os.environ['BD_PORT']=1521
-
-
-
-
-
-
+os.environ['BD_HOST'] = 'localhost'
+os.environ['BD_PORT'] = '1521'
