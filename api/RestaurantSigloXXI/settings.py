e05d35b471040955dd1bf0e2a29c52748282961e
Neatix <yu.uribe@duocuc.cl>
2022-11-29 01:24:00 -0300

    asd

--- api/RestaurantSigloXXI/settings.py
+++ api/RestaurantSigloXXI/settings.py
@@ -88,22 +88,22 @@ WSGI_APPLICATION = 'RestaurantSigloXXI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.oracle',
-#        'NAME': environ.get('BD_NAME'),
-#        'USER': environ.get('BD_USER'),
-#        'PASSWORD': environ.get('BD_PASSWORD'),
-#        'HOST': environ.get('BD_HOST'),
-#        'PORT': environ.get('BD_PORT'),
-#    }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.oracle',
+       'NAME': environ.get('BD_NAME'),
+       'USER': environ.get('BD_USER'),
+       'PASSWORD': environ.get('BD_PASSWORD'),
+       'HOST': environ.get('BD_HOST'),
+       'PORT': environ.get('BD_PORT'),
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
